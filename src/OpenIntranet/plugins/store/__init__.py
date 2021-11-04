5f5990339eb164de64ae33f51b23744d3ee180b5
Roman Dvorak <romandvorak@mlab.cz>
2021-11-04 16:36:50 +0100

    uvodni verze samoinstalacniho skriptu #166, pripravuje sklad pro import dat z externich zdroju

--- src/OpenIntranet/plugins/store/__init__.py
+++ src/OpenIntranet/plugins/store/__init__.py
@@ -6,6 +6,8 @@ from .popover import get_plugin_handlers as handlers_3
 from .. import BaseHandler
 
 
+plugin_version = 1
+
 def get_plugin_handlers():
     return handlers() + handlers_2() + handlers_3()
 
@@ -22,3 +24,21 @@ def get_plugin_info():
         ],
         "role": ["sudo", "sudo-store", "store-manager", "store-user"]
     }
+
+def plugin_init(db):
+    actual_data = db.intranet_plugins.find_one({'_id': 'store'})
+
+    if not actual_data: # Prvni spusteni pluginu... 
+        print("Plugin 'store' byl spuštěn poprvé. Pokusím se ho nainstalovat.. ")
+
+        structure = {
+            '_id': 'store',
+            'version': plugin_version,
+            'data': { 'data_import': {}}
+        }
+        db.intranet_plugins.insert_one(structure)
+
+    else:
+        actual_version = actual_data['version']
+
+
